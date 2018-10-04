c646eb1da956bb8dff9f42664514086c5789bbda
Felipe Panegalli <felipe.panegalli@gmail.com>
2018-10-03 23:03:54 -0300

    Adicionado validações do user->POST e Criado a primeira etapa do login

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,37 @@
 |
 */
 
+use Illuminate\Http\Request;
+
 $router->get('/', function () use ($router) {
     return $router->app->version();
+});
+
+$router->group(['prefix' => 'api/v1'], function () use ($router) {
+    $router->post('/users', function (Request $request) {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:190|unique:users',
+            'password' => 'required|min:8|max:20|confirmed'
+        ]);
+        $data = $request->all();
+        $data['password'] = \Hash::make($data['password']);
+        $model = \App\User::create($data);
+        return response()->json($model, 201);
+    });
+
+    $router->post('/login', function (Request $request) {
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+        $email = $request->get('email');
+        $password = $request->get('password');
+        $user = \App\User::where('email', '=', $email)->first();
+        if (!$user || !\Hash::check($password, $user->password)) {
+            return response()->json(['message' => 'Invalid credentilas'], 400);
+        } else {
+            return response()->json(['message' => 'Congratulations'], 200);
+        }
+    });
 });
